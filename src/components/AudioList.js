02b31d6856079b42b2abb54f0cf1fdad52e6d742
csw10141 <cjstjddn10@naver.com>
2024-08-31 15:43:18 +0900

    modified:   src/components/AudioList.js

--- src/components/AudioList.js
+++ src/components/AudioList.js
@@ -6,13 +6,13 @@ function AudioList({ audios }) {
     <div className="row">
       {audios.map(audio => (
         <div key={audio.id} className="col-md-4 mb-4">
-          <div className="card h-100">
-            <div className="card-body">
-            <Link to={`/audio/${audio.id}`}>
-              <h5 className="card-title">{audio.title}</h5>
+            <Link to={`/audio/${audio.id}`} style={{color:'#000', textDecoration:'none'}}>
+                <div className="card h-100">
+                    <div className="card-body">
+                    <h5 className="card-title">{audio.title}</h5>
+                    </div>
+                </div>
             </Link>
-            </div>
-          </div>
         </div>
       ))}
     </div>
