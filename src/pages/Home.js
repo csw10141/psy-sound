2a38d2a9c9cae757433f7f497714ff109f30c158
csw10141 <cjstjddn10@naver.com>
2024-08-31 12:54:24 +0900

    renamed:    "public/audio/12_\354\202\254\353\236\214\354\235\230 \353\247\210\354\235\214.mp3" -> public/audio/sample12.mp3 	renamed:    "public/audio/03_\353\221\220 \353\202\230\353\254\264.mp3" -> public/audio/sample3.mp3 	renamed:    "public/audio/05_\354\213\240\352\270\260\353\243\250.mp3" -> public/audio/sample5.mp3 	renamed:    "public/audio/06_\354\213\254\355\214\220 \352\267\270\353\246\254\352\263\240 \352\265\254\354\233\220.mp3" -> public/audio/sample6.mp3 	renamed:    public/audio/07_Tower of BABLE.mp3 -> public/audio/sample7.mp3 	renamed:    "public/audio/08_\353\213\254\354\275\244\355\225\234 \354\234\240\355\230\271.mp3" -> public/audio/sample8.mp3 	modified:   src/pages/Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,12 +5,12 @@ function Home() {
   const [audios, setAudios] = useState([
     { id: 1, title: "01_Fallen angel", url: "/audio/sample1.mp3" },
     { id: 2, title: "02_실낙원", url: "/audio/sample2.mp3" },
-    { id: 3, title: "03_두 나무", url: "/audio/03_두 나무.mp3" },
-    { id: 4, title: "05_신기루", url: "/audio/05_신기루.mp3" },
-    { id: 5, title: "06_심판 그리고 구원", url: "/audio/06_심판 그리고 구원.mp3" },
-    { id: 6, title: "07_Tower of BABLE", url: "/audio/07_Tower of BABLE.mp3" },
-    { id: 7, title: "08_달콤한 유혹", url: "/audio/08_달콤한 유혹.mp3" },
-    { id: 8, title: "12_사람의 마음", url: "/audio/12_사람의 마음.mp3" },
+    { id: 3, title: "03_두 나무", url: "/audio/sample3.mp3" },
+    { id: 4, title: "05_신기루", url: "/audio/sample5.mp3" },
+    { id: 5, title: "06_심판 그리고 구원", url: "/audio/sample6.mp3" },
+    { id: 6, title: "07_Tower of BABLE", url: "/audio/sample7.mp3" },
+    { id: 7, title: "08_달콤한 유혹", url: "/audio/sample8.mp3" },
+    { id: 8, title: "12_사람의 마음", url: "/audio/sample12.mp3" },
   ]);
 
   return (
