c60deb090cee8220a45efad3c75a3534ffd80fac
csw10141 <cjstjddn10@naver.com>
2024-08-31 20:01:58 +0900

    modified:   src/components/AudioPlayer.js

--- src/components/AudioPlayer.js
+++ src/components/AudioPlayer.js
@@ -4,36 +4,29 @@ function AudioPlayer({ src }) {
   const audioRef = useRef(null);
 
   useEffect(() => {
+    // 세션 스토리지에서 재생 상태를 가져옴
     const wasPlaying = sessionStorage.getItem('isPlaying') === 'true';
-    const savedTime = sessionStorage.getItem('currentTime');
-    if (savedTime !== null) {
-      audioRef.current.currentTime = parseFloat(savedTime);
-    }
     if (wasPlaying) {
       audioRef.current.play();
     }
 
+    // 재생이 시작되면 세션 스토리지에 저장
     const handlePlay = () => {
       sessionStorage.setItem('isPlaying', 'true');
     };
 
+    // 재생이 중지되면 세션 스토리지에 저장
     const handlePause = () => {
       sessionStorage.setItem('isPlaying', 'false');
-    };
-
-    const handleTimeUpdate = () => {
-      sessionStorage.setItem('currentTime', audioRef.current.currentTime);
     };
 
     const audioElement = audioRef.current;
     audioElement.addEventListener('play', handlePlay);
     audioElement.addEventListener('pause', handlePause);
-    audioElement.addEventListener('timeupdate', handleTimeUpdate);
 
     return () => {
       audioElement.removeEventListener('play', handlePlay);
       audioElement.removeEventListener('pause', handlePause);
-      audioElement.removeEventListener('timeupdate', handleTimeUpdate);
     };
   }, []);
 
