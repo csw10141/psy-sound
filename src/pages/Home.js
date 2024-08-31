6d5caa0f0f921317a5a46da39a59fda44c8ad02a
csw10141 <cjstjddn10@naver.com>
2024-08-31 14:06:45 +0900

    new file:   public/images/sample1.png 	modified:   src/pages/Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,14 +3,14 @@ import AudioList from '../components/AudioList';
 
 function Home() {
   const [audios, setAudios] = useState([
-    { id: 1, title: "01_Fallen angel", url: "/audio/sample1.mp3", img: "/images/sample1.jpg",description: "This is the first sample audio description."},
-    { id: 2, title: "02_실낙원", url: "/audio/sample2.mp3", img: "/images/sample1.jpg",description: "This is the first sample audio description."},
-    { id: 3, title: "03_두 나무", url: "/audio/sample3.mp3", img: "/images/sample1.jpg",description: "This is the first sample audio description."},
-    { id: 4, title: "05_신기루", url: "/audio/sample5.mp3", img: "/images/sample1.jpg",description: "This is the first sample audio description."},
-    { id: 5, title: "06_심판 그리고 구원", url: "/audio/sample6.mp3", img: "/images/sample1.jpg",description: "This is the first sample audio description."},
-    { id: 6, title: "07_Tower of BABLE", url: "/audio/sample7.mp3", img: "/images/sample1.jpg",description: "This is the first sample audio description."},
-    { id: 7, title: "08_달콤한 유혹", url: "/audio/sample8.mp3", img: "/images/sample1.jpg",description: "This is the first sample audio description."},
-    { id: 8, title: "12_사람의 마음", url: "/audio/sample12.mp3", img: "/images/sample1.jpg",description: "This is the first sample audio description."},
+    { id: 1, title: "01_Fallen angel", url: "/audio/sample1.mp3", img: "/images/sample1.png",description: "This is the first sample audio description."},
+    { id: 2, title: "02_실낙원", url: "/audio/sample2.mp3", img: "/images/sample1.png",description: "This is the first sample audio description."},
+    { id: 3, title: "03_두 나무", url: "/audio/sample3.mp3", img: "/images/sample1.png",description: "This is the first sample audio description."},
+    { id: 4, title: "05_신기루", url: "/audio/sample5.mp3", img: "/images/sample1.png",description: "This is the first sample audio description."},
+    { id: 5, title: "06_심판 그리고 구원", url: "/audio/sample6.mp3", img: "/images/sample1.png",description: "This is the first sample audio description."},
+    { id: 6, title: "07_Tower of BABLE", url: "/audio/sample7.mp3", img: "/images/sample1.png",description: "This is the first sample audio description."},
+    { id: 7, title: "08_달콤한 유혹", url: "/audio/sample8.mp3", img: "/images/sample1.png",description: "This is the first sample audio description."},
+    { id: 8, title: "12_사람의 마음", url: "/audio/sample12.mp3", img: "/images/sample1.png",description: "This is the first sample audio description."},
   ]);
 
   return (
